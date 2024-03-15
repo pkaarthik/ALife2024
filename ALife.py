4cf4e6773a469f421b213402dd4d52fb5c1892ce
pkaarthik <76706363+pkaarthik@users.noreply.github.com>
2024-03-15 10:18:34 -0500

    Add files via upload

--- ALife.py
+++ ALife.py
@@ -4,7 +4,7 @@
 # In[1]:
 
 
-import mujoco
+import mujoco as mj
 import dm_control.mujoco
 from mujoco import viewer
 import random
@@ -39,7 +39,7 @@ def evolution(xml, gen, bodies_per_gen, iterations_body):
     while i<gen:
         while j<bodies_per_gen:
             fitness = mutate(xml,i,j, iterations_body)                    #Mutate the model, simulate and determine the fitness of the model       
-            xml_list.append("new.xml")
+            xml_list.append(f"Gen{i}Body{j}.xml")
             if fitness > best_body_fitness:
                 best_body_fitness = fitness
                 best_body = j
@@ -73,10 +73,92 @@ def evolution(xml, gen, bodies_per_gen, iterations_body):
 
 ### MUTATE ###
 def mutate(best_xml_file,i,j, iterations_body):
-    tree = ET.parse(best_xml_file)
-    root = tree.getroot()
-    if (random.uniform(0,1)>0.25):  #mutate if random generation is >0.25 to throw further variability into each generation
+
+    num = random.uniform(0,1)
+    print(num)
+    if num<=0.5:
         
+        tree = ET.parse(best_xml_file)
+        root = tree.getroot()
+    
+        mujoco = ET.Element('mujoco')
+        worldbody = ET.SubElement(mujoco, 'worldbody')
+        light = ET.SubElement(worldbody, 'light', diffuse=".5 .5 .5", pos="0 0 3", dir="0 0 -1")
+        plane = ET.SubElement(worldbody, 'geom', name="ground", type="plane", size="50 50 0.1", rgba=".9 .9 .9 1")
+        actuator = ET.SubElement(mujoco, 'actuator')
+        bodyframe= ET.SubElement(worldbody, 'body', name="geodude_body", pos="0 0 1") 
+    
+        joint_geodude = ET.SubElement(bodyframe, 'joint', type = "free")
+        body_geodude = ET.SubElement(bodyframe, 'body', name = "main_body", pos = "0 0 0")
+        geom_geodude = ET.SubElement(bodyframe, 'geom', type = "sphere", size = "0.12", rgba = "1 0 0 1", pos = "0 0 0", mass ="5")
+    
+        num_arms = 0
+        arm_range = []
+        arm_gear = []
+        arm_mass = []
+        
+        
+        for body in root.findall('.//body'):
+            if body.get('name', '').startswith('arm'):
+                num_arms+=1
+    
+        for body in root.findall('.//joint'):
+            if body.get('name', '').startswith('arm'):
+                arm_range.append(body.get('range'))
+    
+        for body in root.findall('.//motor'):
+            if body.get('name', '').startswith('arm'):
+                arm_gear.append(body.get('gear'))
+    
+
+        num_arms+=1
+        count = 1
+        pos0 = random.uniform(0,0.5)
+        pos1 = random.uniform(0,0.5)
+        pos2 = 0
+
+        while count <= num_arms:
+            if count == 1:
+                pos0 = -0.2
+                pos1 = 0
+            elif count == 2:
+                pos0 = 0.2
+                pos1 = 0
+            else:
+                pos0 = random.uniform(-0.1,0.1)
+                pos1 = random.uniform(-0.1,0.1)
+            arm = ET.SubElement(bodyframe, 'body', name = "arm"+str(count), pos =f"{pos0} {pos1} {pos2}")
+            #print("Success 1")       
+            joint_geom = ET.SubElement(arm, 'geom', name="arm" + str(count), type="capsule", size="0.05 0.02",  rgba="0 1 0 1", mass="0.01")
+            #print("Success 2")
+            joint_hinge = ET.SubElement(arm, 'joint', name="arm" + str(count), type="hinge", axis="1 0 0")
+            #print("Success 3")
+            if count == 1:
+                body_hand = ET.SubElement(arm, 'body', name = "hand" + str(count), pos = "-0.08 -0.05 0")
+                geom_hand = ET.SubElement(body_hand, 'geom', type = "sphere", size = "0.03", rgba = "0 0 1 1", mass = "1")
+            elif count == 2:
+                body_hand = ET.SubElement(arm, 'body', name = "hand" + str(count), pos = "0.08 -0.05 0")
+                geom_hand = ET.SubElement(body_hand, 'geom', type = "sphere", size = "0.03", rgba = "0 0 1 1", mass = "1")
+                
+            motor = ET.SubElement(actuator, 'motor', name ="arm" + str(count), joint = "arm" + str(count), gear = "0.2", ctrllimited = "true", ctrlrange = "-60 60")
+            #print("Success 4")  
+            count+=1 
+        
+        with open(f"Gen{i}Body{j}.xml", 'wb') as file:
+            tree = ET.ElementTree(mujoco)
+            #tree.write(file) 
+            tree.write(f"Gen{i}Body{j}.xml")
+
+        
+        num_arms = 0
+    
+
+    
+    else:
+        
+        tree = ET.parse(best_xml_file)
+        root = tree.getroot()
+        #mutate if random generation is >0.25 to throw further variability into each generation        
         ###Modify main body size and mass and arm size and mass
         body_geom = root.find(".//body[@name='geodude_body']/body/geom")
         body_geom.set('mass', str(float(body_geom.get('mass'))*random.uniform(0.85,1.15))) #body mass change
@@ -89,19 +171,24 @@ def mutate(best_xml_file,i,j, iterations_body):
         arm_geom = root.find(".//body[@name='arm2']/body/geom")
         arm_geom.set('mass', str(float(arm_geom.get('mass'))*random.uniform(0.85,1.15)))  #arm mass change
         arm_geom.set('size', str(float(arm_geom.get('size'))*random.uniform(0.85,1.15)))  #arm size change
-           
-    tree.write(f"Gen{i}Body{j}.xml")   #write to new xml files
-    tree.write("new.xml") 
 
-    model = mujoco.MjModel.from_xml_path("new.xml") #read to mujoco model viewer
-    data = mujoco.MjData(model)  
+        arm_geom = root.find(".//body[@name='arm1']/body/geom")
+        arm_mass = arm_geom.get('mass')  #arm mass change
+        arm_size = arm_geom.get('size')  #arm size change
+
+         
+        tree.write(f"Gen{i}Body{j}.xml")   #write to new xml files
+        #tree.write("new.xml") 
+
+    model = mj.MjModel.from_xml_path(f"Gen{i}Body{j}.xml") #read to mujoco model viewer
+    data = mj.MjData(model)  
      
     joint_range = 45 #arm motion range
     
     iterations = 0 #iteration counter
     i=0
 
-    with mujoco.viewer.launch_passive(model, data) as viewer:
+    with mj.viewer.launch_passive(model, data) as viewer:
         viewer.cam.azimuth = 45                                       #View parameters
         viewer.cam.elevation = -20
         viewer.cam.distance = 10
@@ -140,10 +227,10 @@ def plot(gen_list, best_gen_fitness):   #plot best fitness of each generation, w
     plt.savefig("fitness.png")
 
 
-# In[5]:
+# In[7]:
 
 
-#evolution("geodude.xml", 10, 20, 5000)
+#evolution("geodude.xml", 20, 20, 5000)
 
 
 # In[6]:
@@ -151,6 +238,12 @@ def plot(gen_list, best_gen_fitness):   #plot best fitness of each generation, w
 
 if __name__ == "__main__":
     evolution("geodude.xml", 10, 100, 5000)
+
+
+# In[ ]:
+
+
+
 
 
 # In[ ]:
